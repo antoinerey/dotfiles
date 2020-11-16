4f9b86c2d0485d8dde81d20436a06160c27eb435
Antoine Rey <antoine.rey@backmarket.com>
2020-11-16 15:05:41 +0100

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,12 +22,21 @@ alias gaa="git add ."
 alias gc="git commit"
 alias gd="git diff --ignore-space-change"
 alias gds="git diff --ignore-space-change --staged"
-alias gco="git checkout"
 alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gl="git log --format='%Cgreen%h%Creset %s - %Cblue%an%Creset' --no-merges"
 alias gb="git branch"
 alias gf="git fetch"
 alias grc="git rebase --continue"
+
+function gco {
+  local ref=$1
+
+  if [ $ref = "-" ]; then
+    git checkout $ref
+  else
+    git checkout -b $1 2> /dev/null || git checkout $1;
+  fi
+}
 
 source $(dirname $0)/aliases.bm.zsh
