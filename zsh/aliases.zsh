176895706212e7afbdfa26a9988766f73505fa09
Antoine Rey <antoine.rey@backmarket.com>
2020-12-01 10:08:35 +0100

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,22 +17,14 @@ alias yt="yarn test"
 
 # git
 alias gs="git status --short"
-alias ga="git add"
 alias gaa="git add ."
 alias gc="git commit"
+alias gco="git checkout"
 alias gd="git diff --ignore-space-change"
 alias gds="git diff --ignore-space-change --staged"
 alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gl="git log --format='%Cgreen%h%Creset %s - %Cblue%an%Creset' --no-merges"
 alias gb="git branch"
-alias gf="git fetch"
-alias grc="git rebase --continue"
-
-function gco {
-  local ref=$1
-
-  git checkout $ref || git checkout -b $ref;
-}
 
 source $(dirname $0)/aliases.bm.zsh
