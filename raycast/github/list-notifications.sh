990ba4743e688b7dbfa4e6c91f0d8196d51ebeab
Antoine Rey <antoine.rey@backmarket.com>
2021-02-16 09:32:24 +0100

    Update

--- raycast/github/list-notifications.sh
+++ raycast/github/list-notifications.sh
@@ -9,6 +9,30 @@
 # @raycast.iconDark ../assets/github-light.png
 # @raycast.packageName Github
 
-notifications=$(gh api notifications | jq length)
+# Fetch notifications from the Github API.
+notifications=$(gh api notifications)
 
-echo "$notifications notifications"
+# Compute the notifications count.
+# See: https://stedolan.github.io/jq/manual/#length
+notifications_count=$(echo $notifications | jq length)
+
+if [ $notifications_count = 0 ]; then
+  echo "☀️";
+fi
+
+# Group every notifications by their repository name.
+# See: https://stedolan.github.io/jq/manual/#group_by(path_expression)
+notifications_grouped_by=$(echo $notifications | jq 'group_by(.repository.name)')
+
+# Transform each notifications group into a stringified overview: "<repository>: <number>".
+# See: https://stedolan.github.io/jq/manual/#map(x),map_values(x)
+notifications_grouped_by_strings=$(echo $notifications_grouped_by | jq 'map(.[0].repository.name + ": " + (length | tostring))')
+
+# Join the array of strings into a single string.
+# See: https://stedolan.github.io/jq/manual/#join(str)
+notifications_count_raw=$(echo $notifications_grouped_by_strings | jq 'join(", ")')
+
+# Remove `"` from the final string, so it looks cleaner in the interface.
+notifications_count_clean=$(echo $notifications_count_raw | sed 's/"//g')
+
+echo $notifications_count_clean
--- raycast/github/list-pull-requests.sh
+++ raycast/github/list-pull-requests.sh
@@ -9,6 +9,15 @@
 # @raycast.iconDark ../assets/github-light.png
 # @raycast.packageName Github
 
-open_pr=$(gh api issues | jq length)
+# Fetch pull requests from the Github API.
+pull_requests=$(gh api issues)
 
-echo "$open_pr pull requests"
+# Compute the pull requests count.
+# See: https://stedolan.github.io/jq/manual/#length
+pull_requests_count=$(echo $pull_requests | jq length)
+
+if [ $pull_requests_count = 0 ]; then
+  echo "☀️";
+fi
+
+echo "$pull_requests_count pull requests";
