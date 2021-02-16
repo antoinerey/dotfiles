e60691fe21d198582d04b45b631205a5be400d8c
Antoine Rey <antoine.rey@backmarket.com>
2021-02-16 11:20:43 +0100

    Update

--- raycast/github/list-pull-requests.sh
+++ raycast/github/list-pull-requests.sh
@@ -20,4 +20,19 @@ if [ $pull_requests_count = 0 ]; then
   echo "☀️";
 fi
 
-echo "$pull_requests_count pull requests";
+# Group every pull requests by their repository name.
+# See: https://stedolan.github.io/jq/manual/#group_by(path_expression)
+pull_requests_grouped_by=$(echo $pull_requests | jq 'group_by(.repository.name)')
+
+# Transform each pull requests group into a stringified overview: "<repository>: <number>".
+# See: https://stedolan.github.io/jq/manual/#map(x),map_values(x)
+pull_requests_grouped_by_strings=$(echo $pull_requests_grouped_by | jq 'map(.[0].repository.name + ": " + (length | tostring))')
+
+# Join the array of strings into a single string.
+# See: https://stedolan.github.io/jq/manual/#join(str)
+pull_requests_count_raw=$(echo $pull_requests_grouped_by_strings | jq 'join(", ")')
+
+# Remove `"` from the final string, so it looks cleaner in the interface.
+pull_requests_count_clean=$(echo $pull_requests_count_raw | sed 's/"//g')
+
+echo $pull_requests_count_clean
