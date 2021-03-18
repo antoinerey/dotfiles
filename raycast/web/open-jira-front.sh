c7f04e56695af9734f9dc264c3e6fc38be537493
Antoine Rey <antoine.rey@backmarket.com>
2021-03-18 10:09:38 +0100

    Update

---
+++ raycast/web/open-jira-front.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Jira – Front
+# @raycast.mode silent
+# @raycast.author Antoine Rey
+# @raycast.icon ../assets/jira.ico
+
+open "https://backmarket.atlassian.net/secure/RapidBoard.jspa?rapidView=108&projectKey=FRONT"
---
+++ raycast/web/open-jira-platform.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Jira – Platform
+# @raycast.mode silent
+# @raycast.author Antoine Rey
+# @raycast.icon ../assets/jira.ico
+
+open "https://backmarket.atlassian.net/jira/software/projects/PLTD/boards/181?assignee=5a8b07c25c7a2233512ac788"
