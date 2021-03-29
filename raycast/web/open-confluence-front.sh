01939ed3fb0637915d98fa0ad2f65783d7ce5ee8
Antoine Rey <antoine.rey@backmarket.com>
2021-03-29 14:39:42 +0200

    Update

---
+++ raycast/web/open-confluence-front.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Confluence – Front
+# @raycast.mode silent
+# @raycast.author Antoine Rey
+# @raycast.icon ../assets/confluence.ico
+
+open "https://backmarket.atlassian.net/wiki/spaces/FRONT/pages"
---
+++ raycast/web/open-confluence-platform.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Confluence – Platform
+# @raycast.mode silent
+# @raycast.author Antoine Rey
+# @raycast.icon ../assets/confluence.ico
+
+open "https://backmarket.atlassian.net/wiki/spaces/PLTD/overview"
