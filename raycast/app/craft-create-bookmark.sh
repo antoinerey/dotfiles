ab2917063433ed5efc0f02c2ce8fb8326ce6f72b
Antoine Rey <antoine.rey@backmarket.com>
2021-06-08 18:04:27 +0200

    Update

--- raycast/app/craft-create-bookmark.sh
+++ raycast/app/craft-create-bookmark.sh
@@ -6,11 +6,12 @@
 # @raycast.mode inline
 
 # Optional parameters:
-# @raycast.icon ../assets/cleanshot-record-screen.png
+# @raycast.icon ../assets/craft.icns
 # @raycast.packageName Craft
 
 parentBlockId=bff09ab7-d379-1bf0-9376-cfd4a27fb9d2
 spaceId=6e679756-2b50-cda7-07a3-68c1974cfc5e
-
 url=$(osascript -e 'tell app "brave" to get the url of the active tab of window 1')
+
 open "craftdocs://createblock?parentBlockId=$parentBlockId&spaceId=$spaceId&content=$url&index=0"
+osascript -e 'tell application "brave" to activate'
