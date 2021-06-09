0200b1b44dcad28d2d03246c9d8214e67962bdb2
Antoine Rey <antoine.rey@backmarket.com>
2021-06-09 10:15:39 +0200

    Update

--- raycast/app/craft-create-bookmark.sh
+++ raycast/app/craft-create-bookmark.sh
@@ -13,5 +13,4 @@ parentBlockId=bff09ab7-d379-1bf0-9376-cfd4a27fb9d2
 spaceId=6e679756-2b50-cda7-07a3-68c1974cfc5e
 url=$(osascript -e 'tell app "brave" to get the url of the active tab of window 1')
 
-open "craftdocs://createblock?parentBlockId=$parentBlockId&spaceId=$spaceId&content=$url&index=0"
-# osascript -e 'tell application "brave" to activate'
+open "craftdocs://createblock?parentBlockId=$parentBlockId&spaceId=$spaceId&content=$url&index=999999"
