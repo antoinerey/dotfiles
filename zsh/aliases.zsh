316a22b057473d4beed20932c2044a5d61b2ef78
Antoine Rey <antoine.rey@backmarket.com>
2021-11-19 15:59:59 +0100

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,26 @@
-# Easily reload ZSH configuration.
-alias rsource="source ~/.zshrc"
-
 alias c="clear"
 alias l="exa -lah"
-alias tree="l --tree --git-ignore --ignore-glob .git"
-
+alias tree="exa -lah --tree --git-ignore --ignore-glob .git"
+alias cat="bat"
+alias grep="rg"
+alias rm="rm -rf"
+alias n="sudo n"
 alias p="pnpm"
 alias y="yarn"
+alias yt="yarn test"
+
+# Reuse the same VSCode window, instead of creating new ones.
+alias code="code -r"
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Git
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 alias g="git"
 alias gl="git log --format='%Cgreen%h%Creset %s - %Cblue%an%Creset' --no-merges"
 alias gd="git diff --ignore-space-change"
 alias gds="git diff --ignore-space-change --staged"
-alias gs="git status"
+alias gs="git status --short"
 alias gaa="git add --all"
 alias gco="git checkout"
 alias gc="git commit"
@@ -20,5 +28,8 @@ alias gpl="git pull"
 alias gp="git push"
 alias gpf="git push --force-with-lease"
 
-# See https://github.com/BurntSushi/ripgrep
-alias grep="rg"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Misc
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+alias rsource="source ~/.zshrc"
